d6c6fbc55ee1149f283c6b272134021431a3ed3a
EFCORITI <106176816+EFCORITI@users.noreply.github.com>
2022-06-08 21:44:41 +0430

    Add files via upload

---
+++ BruteForceHashing.py
@@ -0,0 +1,40 @@
+import hashlib
+import termcolor2
+import termcolor
+from colorama import Fore
+
+
+# efcoriti
+
+#Demonstrates the difference between two types of hashing, SHA1 and Bcrypt
+print(Fore.BLUE)
+print("""
+  ____                _               _   _           _      / ___|_ __ __ _  ___| | _____ _ __  | | | | __ _ ___| |__  | |   | '__/ _` |/ __| |/ / _ \ '__| | |_| |/ _` / __| '_ \ | |___| | | (_| | (__|   <  __/ |    |  _  | (_| \__ \ | | | \____|_|  \__,_|\___|_|\_\___|_|    |_| |_|\__,_|___/_| |_|
+
+""")
+print(termcolor2.colored('id tel and rubika >> @efcoriti ',color = "cyan"))
+print(termcolor2.colored('channel telegram : @efcoriti_programmer' , color = "cyan"))
+print("\n\n\n")
+password = input(termcolor2.colored('Input the password to hash\n> ',color = "red"))
+print(Fore.YELLOW)
+print("\nSHA1===>>\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.sha1(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print(Fore.GREEN)
+print("\n")
+print("\nMD5===>>\n")
+
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.md5(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+
+
+print("\n\n\n\n\n\n\n\n\n\n\n\n")
+print(Fore.BLUE)
+print("""coded by efcoriti
+       goodbye""")
